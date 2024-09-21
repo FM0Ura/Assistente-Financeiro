332c7479e1af9d750d5d9d85f3ad55df7ae08ef6
Felipe Moura <Felipe Moura>
2024-09-21 09:52:31 -0300

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FinanceAssistant;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<Transaction> Transactions { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Transactions = new ObservableCollection<Transaction>();
+        TransactionsListView.ItemsSource = Transactions;
+    }
+
+    private void OnAddTransactionClicked(object sender, EventArgs e)
+    {
+        var description = DescriptionEntry.Text;
+        var amount = decimal.Parse(AmountEntry.Text);
+
+        Transactions.Add(new Transaction { Description = description, Amount = amount });
+
+        DescriptionEntry.Text = string.Empty;
+        AmountEntry.Text = string.Empty;
+    }
+}
+
+public class Transaction
+{
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+}
